d51d736882281aeae3f14d8d8d6dea2d473767e3
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-19 08:40:17 -0700

    Fixing errors

--- _printf.c
+++ _printf.c
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 		va_start(args, format);
 		for (i = 0; format[i] != '\0'; i++)
 		{
-			if (format[i - 1] == '%')
+			if (format[i - 1] == '%' && format[i - 2] != '%')
 			{
 				switch (format[i])
 				{
