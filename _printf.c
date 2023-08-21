70603d55b392c4119aeb006c4d96a0a414fe4161
Ayanda Ngubane <ayanda4me@gmail.com>
2023-08-21 05:47:03 -0700

    Trial code

--- _printf.c
+++ _printf.c
@@ -28,7 +28,7 @@ int _printf(const char *format, ...)
 				length++;
 				i++;
 			}
-			else if (format[i + 1] == " ")
+			else if (format[i + 1] == ' ')
 				return (-1);
 			else if (format[i + 1] != '\0')
 			{
