66c78343b29cad7c52cfcbd693d895e40d3c13d7
haylo237 <njikeabednego@hotmail.com>
2023-08-22 05:13:26 +0000

    restructuring

---
+++ fs_functions.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * print_num - prints number
+ *
+ * Return: integer length
+ */
+int print_num(int num)
+{
+	int len = 0, rev = 0;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	while (num > 0)
+	{
+		rev = rev * 10 + num % 10;
+		num /= 10;
+	}
+
+	while (rev > 0)
+	{
+		_putchar('0' + rev % 10);
+		rev /= 10;
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * print_str - prints strings
+ * @str: string pointer
+ * Return: string length
+ */
+int print_str(const char *str)
+{
+	int len = 0;
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	while (*str)
+	{
+		_putchar(*str);
+		len++;
+		str++;
+	}
+
+	return (len);
+}
+
+/**
+ * print_bin - prints binary
+ * @num: unsigned int
+ * Return: binary length
+ */
+int print_bin(unsigned int num)
+{
+	int len = 0, bit_pos = 31;
+
+	if (num == 0)
+	{
+		_putchar('0');
+		return (1);
+	}
+
+	while (!((num >> bit_pos) & 1))
+	{
+		bit_pos--;
+	}
+
+	while (bit_pos >= 0)
+	{
+		_putchar((num >> bit_pos) & 1 ? '1' : '0');
+		len++;
+		bit_pos--;
+	}
+
+	return (len);
+}
--- main.h
+++ main.h
@@ -7,6 +7,8 @@
 int _printf(const char *format, ...);
 int process_format_specifier(va_list args, char specifier);
 int print_num(int num);
+int print_str(const char *str);
+int print_bin(unsigned int num);
 int _putchar(char c);
 
 #endif
